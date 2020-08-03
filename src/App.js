421e968f17276e33e008eb4b63e27f67a9bb9fd8
Josh Justice <josh.justice@accesscfa.com>
2020-08-03 09:29:42 -0400

    Simplify content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, {useEffect} from 'react';
-import logo from './logo.svg';
 import xhrPromise from './xhrPromise';
-import './App.css';
 
 function App() {
   useEffect(() => {
@@ -12,20 +10,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React</h1>
     </div>
   );
 }
